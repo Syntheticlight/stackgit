33fdbcf04088f398f59a6c03d3188f0d225d2b58
Syntheticlight <hownet123@outlook.com>
2025-12-20 02:25:06 +0800

    fix: remove baidu analytics and refactor language switching with store watcher

---
+++ static/js/channel.fov6R4jj.js
@@ -0,0 +1,1 @@
+import{aI as a,aJ as s}from"../index-DqQ9Itum.js";const o=(o,r)=>a.lang.round(s.parse(o)[r]);export{o as c};
---
+++ static/js/clone.CulsbWZ9.js
@@ -0,0 +1,1 @@
+import{c as r}from"./graph.BLKmpklX.js";function o(o){return r(o,4)}export{o as c};
